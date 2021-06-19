a4fa2b755d7d24c0330f3f6a856403f0ebc89096
Tarikul-islam-shazan <Tarikul@brainstation23.com>
2021-06-20 02:46:46 +0600

    add privacy policy and apiurl update

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019
+      Created with ♥ by <b>SIBUN</b> 2021
     </span>
-    <div class="socials">
+    <div style="cursor: pointer;" (click)="goToPrivacyPage()">
+      <i class="fa fa-shield-alt"></i>
+      Privacy policy
+    </div>
+
+    <!-- <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
       <a href="#" target="_blank" class="ion ion-social-facebook"></a>
       <a href="#" target="_blank" class="ion ion-social-twitter"></a>
       <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
+    </div> -->
   `,
 })
 export class FooterComponent {
+
+  constructor(private router: Router){}
+
+  goToPrivacyPage() {
+    this.router.navigateByUrl('/pages/privacy-policy');
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 export const environment = {
-  serviceUrl: 'http://3.137.137.88:3000/',
+  serviceUrl: 'http://3.141.122.29:3000/',
   production: true,
 };
